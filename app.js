6c06234c417a8583719d748a130f76800004498b
Matthew Lin <mlin2778@gmail.com>
2022-01-05 15:06:10 -0600

    Add .env and .gitignore

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://mlin2778:Sn3akyninjaturtlez123@cluster0.cptit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
